c048721fc41d31cb59d57caf025f165030e4c544
Dinesh Deva <dinesh.deva@gmail.com>
2019-04-27 12:33:09 +0530

    Created folders

---
+++ sort/sort.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : sort.c
+ Author      : Dinesh Deva
+ Version     :
+ Copyright   : Dinesh Deva
+ Description : Program to sort array of integers
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ARRAYMAX 1000
+
+
+
+
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
